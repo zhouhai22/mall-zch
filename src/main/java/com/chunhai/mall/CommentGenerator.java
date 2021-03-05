909649b3864561021fc483581077e1a69a95967b
zhouchunhai <zhouchunhai@cmss.chinamobile.com>
2021-03-05 14:47:43 +0800

    Spring Boot整合Mybatis,并利用Mybatis Generator生成代码

---
+++ src/main/java/com/chunhai/mall/CommentGenerator.java
@@ -0,0 +1,50 @@
+package com.chunhai.mall;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+
+/**
+ * 自定义注释生成器
+ */
+public class CommentGenerator extends DefaultCommentGenerator {
+    private boolean addRemarkComments = false;
+    private static final String EXAMPLE_SUFFIX="Example";
+    private static final String MAPPER_SUFFIX="Mapper";
+    private static final String API_MODEL_PROPERTY_FULL_CLASS_NAME="io.swagger.annotations.ApiModelProperty";
+
+    /**
+     * 设置用户配置的参数
+     * @param properties
+     */
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            if (remarks.contains("\"")) {
+                remarks = remarks.replace("\"", "'");
+            }
+            field.addJavaDocLine("@ApiModelProperty(value = \"" + remarks + "\")");
+        }
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        if (!compilationUnit.getType().getFullyQualifiedName().equals(MAPPER_SUFFIX)
+                && !compilationUnit.getType().getFullyQualifiedName().equals(EXAMPLE_SUFFIX)) {
+            compilationUnit.addImportedType(new FullyQualifiedJavaType(API_MODEL_PROPERTY_FULL_CLASS_NAME));
+        }
+    }
+}
