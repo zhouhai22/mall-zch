1bc5bb3a975e330a5eaa77429b827abed270ef11
zhouchunhai <zhouchunhai@cmss.chinamobile.com>
2021-03-05 18:02:04 +0800

    聚合工程

---
+++ mall-zch/src/main/java/com/chunhai/mall/controller/PmsBrandController.java
@@ -0,0 +1,9 @@
+package com.chunhai.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/brand")
+public class PmsBrandController {
+}
